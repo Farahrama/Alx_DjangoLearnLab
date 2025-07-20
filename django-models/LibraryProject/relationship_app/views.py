83344a5c978a7df91960153d5e8284b4eba27d6d
Farahrama <rafarah507@gmail.com>
2025-07-20 20:06:09 +0300

    django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render
 from .models import Book
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
