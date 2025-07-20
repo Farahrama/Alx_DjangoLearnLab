f03a6588b31006be19100ede1ea46b3bc4681148
Farahrama <rafarah507@gmail.com>
2025-07-20 20:02:57 +0300

    django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render
 from .models import Book
 def list_books(request):
@@ -11,9 +14,6 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def user_login(request):
     if request.method == 'POST':
@@ -24,7 +24,7 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')  # غيريها حسب اسم الصفحة الرئيسية عندك
+                return redirect('home')  
     else:
         form = AuthenticationForm()
     return render(request, 'relationship_app/login.html', {'form': form})
@@ -34,8 +34,7 @@ def user_register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')  # يرجع لتسجيل الدخول بعد التسجيل
-    else:
+            return redirect('login')
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
