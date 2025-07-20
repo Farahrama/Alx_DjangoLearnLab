9ce6b935c138b5a07ef2fd153d9c18677204c5a2
Farahrama <rafarah507@gmail.com>
2025-07-20 20:59:32 +0300

    django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,26 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
     def __str__(self):
-        return self.name
+        return self.title
+
+# Library model
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
+
     def __str__(self):
         return self.name
+
+# Librarian model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='librarians')
 
     def __str__(self):
         return self.name
+
+# UserProfile model
 class UserProfile(models.Model):
     ROLE_CHOICES = (
         ('Admin', 'Admin'),
@@ -33,27 +52,9 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import UserProfile
 
+# Signal to auto-create UserProfile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
-
-    def __str__(self):
-        return self.title
