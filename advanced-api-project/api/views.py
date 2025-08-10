85928f09454d788701f265e4951aff70fcb8e01b
Farahrama <rafarah507@gmail.com>
2025-08-10 16:58:47 +0300

    FR

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter  # Ensure SearchFilter and OrderingFilter are correctly imported
@@ -17,4 +16,28 @@ class ListView(generics.ListAPIView):
     filterset_fields = ['title', 'author', 'publication_year']  # Filtering fields
     search_fields = ['title', 'author']  # Search fields
     ordering_fields = ['title', 'publication_year']  # Ordering fields
-# Create your views here.
+
+    # ListView supports filtering by title, author, and publication year
+    # Search is enabled on the title and author fields
+    # Ordering is enabled by title and publication year, including descending order
+
+
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Read-only for unauthenticated users
+
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
